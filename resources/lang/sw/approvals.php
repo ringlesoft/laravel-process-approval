6a6d9ce33e0466fa8db36f69a25b6950dc82d997
David Ringle <ringunger@gmail.com>
2024-07-05 15:55:57 +0300

    Introduced Translations

--- resources/lang/sw/approvals.php
+++ resources/lang/sw/approvals.php
@@ -33,5 +33,5 @@ return [
     'waiting_for_approval_from' => 'Inasubiri idhini kutoka',
     'write_comment' => 'Andika maoni yako ya idhini',
     'you_can_approve_this' => 'Unaweza kuidhinisha hii kama',
-    'you_can_submit' => 'Unaweza kuwasilisha hati hii kwa idhini.',
+    'you_can_submit' => 'Unaweza kuwasilisha hati hii kwa ajili ya kuidhinishwa.',
 ];
