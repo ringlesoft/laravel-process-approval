593264c5e0615afd9ae67c0d0a5530fc615a7c9c
David Ringle <ringunger@gmail.com>
2024-07-15 20:27:51 +0300

    WIP

--- src/Traits/Approvable.php
+++ src/Traits/Approvable.php
@@ -568,7 +568,6 @@ trait Approvable
             try {
                 DB::beginTransaction();
                 $lastApproval->delete();
-                $lastApproval->delete();
                 $statusesCollection = ApprovalStatusStepData::collectionFromApprovable($this);
                 $updatedStatuses = $statusesCollection->map(function (ApprovalStatusStepData $item) use ($lastApproval) {
                     if ($item->belongsToApproval($lastApproval)) {
