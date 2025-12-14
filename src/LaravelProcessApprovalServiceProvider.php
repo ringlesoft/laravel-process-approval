076e867b9633207a6c79093015d9768f1e4d02f9
David Ringle <ringunger@gmail.com>
2025-12-14 13:15:20 +0300

    New features for the Service Provider

--- src/LaravelProcessApprovalServiceProvider.php
+++ src/LaravelProcessApprovalServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 use RingleSoft\LaravelProcessApproval\Console\Commands\FlowCommand;
+use RingleSoft\LaravelProcessApproval\Console\Commands\InstallCommand;
 use RingleSoft\LaravelProcessApproval\Console\Commands\StepCommand;
 use RingleSoft\LaravelProcessApproval\View\Components\ApprovalActions;
 use RingleSoft\LaravelProcessApproval\View\Components\ApprovalStatusSummary;
@@ -15,6 +16,10 @@ class LaravelProcessApprovalServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->alias(Facades\ProcessApproval::class, 'ProcessApproval');
+
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/process_approval.php', 'process_approval'
+        );
     }
 
     public function boot(): void
@@ -27,17 +32,18 @@ class LaravelProcessApprovalServiceProvider extends ServiceProvider
         if (function_exists('resource_path')) {
             View::prependNamespace('ringlesoft-process-approval', resource_path('views/vendor/ringlesoft/process_approval'));
         }
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        if (config('process_approval.load_migrations', true)) {
+            $migrationsPath = config('process_approval.use_uuids', false)
+                ? __DIR__ . '/../database/migrations_uuid'
+                : __DIR__ . '/../database/migrations';
+            $this->loadMigrationsFrom($migrationsPath);
+        }
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         $this->publishItems();
 
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/process_approval.php', 'process_approval'
-        );
-
         if ($this->app->runningInConsole()) {
             $this->commands([
-//                InstallCommand::class,
+                InstallCommand::class,
                 FlowCommand::class,
                 StepCommand::class,
             ]);
@@ -57,6 +63,10 @@ class LaravelProcessApprovalServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../database/migrations' => database_path('migrations'),
         ], 'approvals-migrations');
+
+        $this->publishes([
+            __DIR__ . '/../database/migrations_uuid' => database_path('migrations'),
+        ], 'approvals-migrations-uuids');
 
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views/vendor/ringlesoft/process_approval'),
