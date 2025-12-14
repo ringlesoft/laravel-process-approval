35ee9e885e4e0b831bef37860258cbd0d42ed108
David Ringle <ringunger@gmail.com>
2025-12-14 12:46:49 +0300

    UUID Trait

---
+++ src/Traits/UsesProcessApprovalUuids.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace RingleSoft\LaravelProcessApproval\Traits;
+
+use Illuminate\Support\Str;
+
+trait UsesProcessApprovalUuids
+{
+    protected function initializeUsesProcessApprovalUuids(): void
+    {
+        if (!config('process_approval.use_uuids', false)) {
+            return;
+        }
+
+        $this->incrementing = false;
+        $this->keyType = 'string';
+    }
+
+    protected static function bootUsesProcessApprovalUuids(): void
+    {
+        static::creating(static function ($model) {
+            if (!config('process_approval.use_uuids', false)) {
+                return;
+            }
+
+            $keyName = $model->getKeyName();
+            if (empty($model->{$keyName})) {
+                $model->{$keyName} = (string) Str::uuid();
+            }
+        });
+    }
+}
